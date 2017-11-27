6d80cc4327dbb4b31e1565309825a2206c7c2058
Trinh Nguyen <xbakatrinhx@yahoo.com>
2017-11-26 18:49:58 -0800

    Initial commit

---
+++ src/hw6.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+struct timeval stop, start;
+int running = 1;
+
+const char * generateTimeStamp() {
+	char returnString[48];
+	gettimeofday(&stop, NULL);
+	int minutes = (stop.tv_sec - start.tv_sec) / 60;
+	int seconds = (stop.tv_sec - start.tv_sec) % 60;
+	int microseconds = (stop.tv_usec - start.tv_usec) / 100;
+	printf("%ld:%02ld.%03ld", minutes, seconds, microseconds);
+	snprintf(returnString, sizeof(returnString), "%ld:%02ld.%03ld\n", minutes, seconds, microseconds);
+	return returnString;
+}
+
+void * programTracker() {
+	while (running == 1) {
+		printf("%s\n",generateTimeStamp());
+		sleep(1);
+	}
+	return NULL;
+}
+
+int main(int argc, char **argv) {
+	gettimeofday(&start, NULL);
+	pthread_t tids[12];
+	pthread_create(&tids[0], NULL, programTracker, NULL);
+	pthread_join(tids[0], NULL);
+	return 0;
+}
